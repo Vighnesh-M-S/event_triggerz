70a01d297e36f1bfc2aae8c8341cf6969781b2c2
VIGHNESH M S <58000117+Vighnesh-M-S@users.noreply.github.com>
2025-04-28 19:36:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -235,6 +235,6 @@ def update_trigger(request: UpdateTriggerRequest, db: Session = Depends(get_db))
     return {"message": f"Trigger '{request.trigger_name}' updated successfully."}
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
